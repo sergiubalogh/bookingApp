a546d25900a152bd3e2e62d8e92ed6113f48b315
Sergiu Balogh <sergiu.balogh@sap.com>
2021-10-24 16:52:25 +0300

    first commit angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    HeaderNavbarComponent
   ],
   imports: [
     BrowserModule,
